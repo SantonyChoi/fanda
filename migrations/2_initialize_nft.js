7d0af1e7c8ee4f24134d47f0ce3de2001a43097b
seunghoon <santony.choi@gmail.com>
2018-07-25 12:45:54 +0800

    Delete unused script

--- migrations/2_initialize_nft.js
+++
@@ -1,5 +0,0 @@
-var LimitedMintableNonFungibleToken = artifacts.require("LimitedMintableNonFungibleToken");
-
-module.exports = function(deployer, network, accounts) {
-  deployer.deploy(LimitedMintableNonFungibleToken, 5);
-};
