e2ffdbd28dff8ab27311e141d4bcda2a70bb7e57
seunghoon <santony.choi@gmail.com>
2018-07-25 17:41:00 +0800

    Add HD wallet

--- truffle.js
+++ truffle.js
@@ -1,4 +1,6 @@
-// Configuration suitable for local development
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic = "upgrade surround surprise cage swim angle page ladder ability boost horror ability upset bread narrow"
+// 0x57e5F8c2f8d8AEd8C73732BAd1b537911e9fA20E
 
 module.exports = {
   networks: {
@@ -6,6 +8,12 @@ module.exports = {
       host: "localhost",
       port: 7545,
       network_id: "*",
-    }
+    },
+    rinkeby: {
+			provider: function() {
+				return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/sZoUPUrIYWbpve0L2tFq", 0);
+			},
+			network_id: 4
+		}
   }
 };
